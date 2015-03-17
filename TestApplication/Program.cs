34fd5119df545ec7fcb14798d694f57d78a495cd
jsturtevant@gmail.com <jsturtevant@gmail.com>
2015-03-17 04:06:17 +0000

    Added Sample Application to test diagnostic against

---
+++ TestApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var enterprise = SpaceShipFactory.CreateSpaceship(ShipTypes.USSEnterprise);
+            Console.WriteLine(enterprise.ShipName);
+
+            SpaceShipFactory.CreateSpaceship(ShipTypes.USSndromeda);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class SpaceShipFactory
+    {
+        public static Ship CreateSpaceship(ShipTypes type)
+        {
+            return new Ship(type.ToString());
+        }
+    }
+
+    public class Ship
+    {
+        public Ship(string name)
+        {
+            this.ShipName = name;
+        }
+
+        public string ShipName { get; } 
+    }
+
+    public enum ShipTypes
+    {
+        USSEnterprise,
+        USSndromeda
+    }
+}
